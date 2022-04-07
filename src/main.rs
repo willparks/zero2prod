a8846b1e890cacf3af8b6084ebaacae5da47a25e
Will Parks <will.parks@launchbadge.com>
2022-04-06 17:42:19 -0700

    update config for production db

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,7 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
         .connect_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(&configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres.");
+        .connect_lazy_with(configuration.database.with_db());
     let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
